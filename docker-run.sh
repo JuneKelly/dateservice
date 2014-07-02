ac84bb47cdc0138380f97213997b072e473eb931
Shane Kilkelly <shane@kilkelly.me>
2014-07-02 19:42:02 +0100

    cleanup

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,17 @@
 #! /usr/bin/env bash
 
 timestamp=`date +%Y%m%d_%H%M%S`
-image_name="dateservice_$timestamp"
-container_name="ds_$timestamp"
+image_name="dateservice"
+container_name="ds"
 
 go build .
 
+echo -e ">> Removing previous container and image"
+sudo docker rm $container_name
+sudo docker rmi $image_name
+
+echo -e ">> Building image"
 sudo docker build -t $image_name .
 
+echo -e ">> Running container"
 sudo docker run -p 3000:3000 --name $container_name $image_name
